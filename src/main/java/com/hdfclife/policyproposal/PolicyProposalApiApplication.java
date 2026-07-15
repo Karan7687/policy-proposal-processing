17d3b3b3bb7c1b7fcb8381772d6294fccc5df99a
User <user@example.com>
2026-07-15 14:51:10 +0530

    chore: set up Spring Boot project with Maven and Java 21

---
+++ src/main/java/com/hdfclife/policyproposal/PolicyProposalApiApplication.java
@@ -0,0 +1,13 @@
+package com.hdfclife.policyproposal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyProposalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyProposalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hdfclife/policyproposal/PolicyProposalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdfclife.policyproposal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyProposalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
